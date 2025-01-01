26fe4df3f373ff634546b827faf8e66b3d0835b5
PabodaWA <wampaboda@gmail.com>
2025-01-01 11:19:11 +0530

    file creating

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="./CSS/header.css">
+    </head>
+    <body>
+        <header>
+            <div class="main_header">
+            
+                <a href="index.php"><img src="Pictures/logo.png" alt="PharmacyX Logo"></a>
+                <nav>
+                <a href="index.php">Home</a>
+                <a href="products.php">Products</a>
+                <a href="aboutUs.php">About</a>
+                <a href="contact.php">Contact</a>
+            </nav>
+            </div>
+        </header>
+    </body>
+</html>
