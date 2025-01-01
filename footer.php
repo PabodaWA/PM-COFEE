f6d8456f6015f59e2597fd00a8693fb25f582944
PabodaWA <wampaboda@gmail.com>
2025-01-01 11:27:03 +0530

    footer

---
+++ footer.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="./CSS/footer.css">
+    </head>
+    <body>
+        <footer>
+        <div class="footer_container">
+            <div class="footer-content">
+                <img src="./Pictures/logo.png" alt="logo" width="100px">
+                <p class="footer-details">177/2,<br>Lotus Road<br>Colombo 06.</p>
+    
+            </div>
+
+        
+            <div class="footer-content">
+                    
+                    <p class="footer-details"><a href="" class="footer-links">&#10148 My Shop</a></p>
+                    <p class="footer-details"><a href="" class="footer-links">&#10148 Review</a></p>
+                    <p class="footer-details"><a href="" class="footer-links">&#10148 T&C</a></p>
+            </div>
+
+            <div class="footer-content">
+                    
+                    <p class="footer-details"><a href="aboutUs.php" class="footer-links">&#10148 About</a></p>
+                    <p class="footer-details"><a href="faq.php" class="footer-links">&#10148 FAQ</a></p>
+                    <p class="footer-details"><a href="contact.php" class="footer-links">&#10148 Contact</a></p>
+            </div>
+
+        </div>
+
+        <hr class="footer-row">
+        <div class="footer-end">
+            <div>
+        <p class="copyright-tag"> Copyright &copy; 2025 <span clss="bold-text">PM-Cofee</span>All right reserved</p>
+        </div>
+        <div class="footer-end-links">
+            <div><a href="t&c.php" clss="footer-end-links-content">Terms & Conditions</a></div>
+        </div>
+        <div><h3>|</h3></div>
+        <div><a href="privacyPolicy.php" class="footer-end-links-content">Privacy Policy</a></div>
+        </div>
+
+        </footer>
+    </body>
+</html>
