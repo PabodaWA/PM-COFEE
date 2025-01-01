ff087d4e41ef2cfce9effc41e11f340c81fcb206
PabodaWA <wampaboda@gmail.com>
2025-01-01 11:47:01 +0530

    header and footer

--- footer.php
+++ footer.php
@@ -22,9 +22,9 @@
 
             <div class="footer-content">
                     
-                    <p class="footer-details"><a href="aboutUs.php" class="footer-links">&#10148 About</a></p>
-                    <p class="footer-details"><a href="faq.php" class="footer-links">&#10148 FAQ</a></p>
-                    <p class="footer-details"><a href="contact.php" class="footer-links">&#10148 Contact</a></p>
+                    <p class="footer-details"><a href="AboutUs.php" class="footer-links">&#10148 About</a></p>
+                    <p class="footer-details"><a href="FAQ.php" class="footer-links">&#10148 FAQ</a></p>
+                    <p class="footer-details"><a href="ContactUs.php" class="footer-links">&#10148 Contact</a></p>
             </div>
 
         </div>
@@ -35,10 +35,10 @@
         <p class="copyright-tag"> Copyright &copy; 2025 <span clss="bold-text">PM-Cofee</span>All right reserved</p>
         </div>
         <div class="footer-end-links">
-            <div><a href="t&c.php" clss="footer-end-links-content">Terms & Conditions</a></div>
+            <div><a href="T&C.php" clss="footer-end-links-content">Terms & Conditions</a></div>
         </div>
         <div><h3>|</h3></div>
-        <div><a href="privacyPolicy.php" class="footer-end-links-content">Privacy Policy</a></div>
+        <div><a href="" class="footer-end-links-content">Privacy Policy</a></div>
         </div>
 
         </footer>
